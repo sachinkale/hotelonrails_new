dea9740d4d625a4bfd26e0495d34ffa6fecf7cee
Sachin Kale <brightspark@gmail.com>
2012-04-09 11:24:30 +0530

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelonrailsNew::Application.config.secret_token = 'e6ae8cea35e3d95ff7a821b43f1f5481be1f405836d6a23822511367c9be1319a8ab263ef154699a994fa60ca3b4a3dd9b62781564bb203aec815c739b38bf58'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+HotelonrailsNew::Application.routes.draw do
+  root :to => "home#index"
+end
